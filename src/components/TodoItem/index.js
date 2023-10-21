7a04c8b16f7bc99ecfbe12708bdccc0635ebac49
Doredla-Charan-Sai <charansai9247@gmail.com>
2023-10-21 15:27:49 +0000

    in TodoItem changed by accessing the props in the render()

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -2,19 +2,19 @@
 import {Component} from 'react'
 import './index.css'
 
-class TodoItem extends Component{
-    const {todoDetails} = this.props 
-
+class TodoItem extends Component {
+  render() {
+    const {todoDetails} = this.props
     const {todo, id} = todoDetails
-
-    render(){
-        return(
-            <li className = "todo-item-cont">
-                <p className="todo-item-para">{todo}</p>
-                <button className="delete-btn" type="button" onClick={}>Delete</button>
-            </li>
-        )
-    }
+    return (
+      <li className="todo-item-cont">
+        <p className="todo-item-para">{todo}</p>
+        <button className="delete-btn" type="button" onClick={}>
+          Delete
+        </button>
+      </li>
+    )
+  }
 }
 
 export default TodoItem
